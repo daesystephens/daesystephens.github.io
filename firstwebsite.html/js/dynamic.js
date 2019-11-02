bcda10f74e2f41d3546a8383cbcf9f11a74e74fa
daesy13 <daesyramos@hotmail.com>
2019-11-02 13:31:39 -0700

    comment extra loop

--- firstwebsite.html/js/dynamic.js
+++ firstwebsite.html/js/dynamic.js
@@ -25,19 +25,19 @@ window.addEventListener("keyup", event => {
 // Adding for loop after getting answer of prompt
 // prompt('Enter a string'); // returns string that was entered
 
-var numIcecream = confirm("Choose a number from 1 to 10 to tell me how many icecreams  you want. And I will tell you wich flavors you will get:");
-numIcecream = parseInt(numIcecream);
-var flavors = ['vanilla', 'chocolate', 'strawberry', 'mango', 'pinapple', 'watermelon', 'cherry', 'bubblegum', 'chocolate mint', 'cookie dough'];
-var answr = '';
+// var numIcecream = confirm("Choose a number from 1 to 10 to tell me how many icecreams  you want. And I will tell you wich flavors you will get:");
+// numIcecream = parseInt(numIcecream);
+// var flavors = ['vanilla', 'chocolate', 'strawberry', 'mango', 'pinapple', 'watermelon', 'cherry', 'bubblegum', 'chocolate mint', 'cookie dough'];
+// var answr = '';
 
-for (var i = 1; i < 11; i++) {
-    if (numIcecream !== i) {
-        alert("Opps, Please try again any a number from 1 to 10");
-    }
-    else {
-        for ( var j = 0; j < flavors.length; j++) {
-            answr += flavors[j] + 'ice cream flavor';<br />
-        }
-        document.getElementById('output').innerHTML = answr;
-    }
-}
+// for (var i = 1; i < 11; i++) {
+//     if (numIcecream !== i) {
+//         alert("Opps, Please try again any a number from 1 to 10");
+//     }
+//     else {
+//         for ( var j = 0; j < flavors.length; j++) {
+//             answr += flavors[j] + 'ice cream flavor';<br />
+//         }
+//         document.getElementById('output').innerHTML = answr;
+//     }
+// }
