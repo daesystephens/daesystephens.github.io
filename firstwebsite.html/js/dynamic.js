528d4583fcb446f0929171cfb1f7b937046fb57a
daesy13 <daesyramos@hotmail.com>
2019-11-02 15:44:50 -0700

    create validation using forms

--- firstwebsite.html/js/dynamic.js
+++ firstwebsite.html/js/dynamic.js
@@ -22,26 +22,48 @@ else {
     alert("Bummer... 🍔")
 }
 
+prompt('Enter a string'); // returns string that was entered
 
-var numIcecream = prompt("Choose a number from 1 to 10 to tell me how many icecreams  you want. And I will tell you wich flavors you will get:");
-num = Number(numIcecream);
-var flavors = ['vanilla', 'chocolate', 'strawberry', 'mango', 'pinapple', 'watermelon', 'cherry', 'bubblegum', 'chocolate mint', 'cookie dough'];
-var answr = 'You will get : ';
+// 1[x]Add validation on one of the user inputs.
+// 2[x]Keep asking until an acceptable value is entered.
+// 3[ ]Prompt the user for how many things they’d like your shop to make.
+// 4[ ]Use a loop in your JavaScript to add items to the page that many times.
+// 5[ ]Can functions make your code more readable ?
 
-if (numIcecream != null){
-    // for (var i = 1; i < 11; i++) {
-        // if (num !== i) {
-        //     numIcecream = prompt("Opps, Please try again another number from 1 to 10");
-        // }
-        // else {
-            for (var k = 0; k < num; k++){
-                    answr += flavors[k] + ' ice cream flavor, ';
-        }
-            alert(answr);
-        // }
-    // }
+// var numIcecream = prompt("Choose a number from 1 to 10 to tell me how many icecreams  you want. And I will tell you wich flavors you will get:");
+// num = Number(numIcecream);
+// var flavors = ['vanilla', 'chocolate', 'strawberry', 'mango', 'pinapple', 'watermelon', 'cherry', 'bubblegum', 'chocolate mint', 'cookie dough'];
+// var answr = 'You will get : ';
+
+// if (numIcecream != null){
+//     // for (var i = 1; i < 11; i++) {
+//         // if (num !== i) {
+//         //     numIcecream = prompt("Opps, Please try again another number from 1 to 10");
+//         // }
+//         // else {
+//             for (var k = 0; k < num; k++){
+//                     answr += flavors[k] + ' ice cream flavor, ';
+//         }
+//             alert(answr);
+//         // }
+//     // }
+// }
+
+function listOfFlavors() {
+    var x, text;
+
+    // Get the value of the input field with id="numb"
+    x = document.getElementById("num").value;
+
+    // If x is Not a Number or less than one or greater than 10
+    if (isNaN(x) || x < 1 || x > 10) {
+        text = "Input not valid";
+    } else {
+        text = "Input OK";
+    }
+    document.getElementById("demo").innerHTML = text;
 }
-// Adding for loop after getting answer of prompt
-// prompt('Enter a string'); // returns string that was entered
+
+
 
 
