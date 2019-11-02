ad92f11a0fd104131becd2f7c595f2b0242ba4f1
daesy13 <daesyramos@hotmail.com>
2019-11-02 13:52:38 -0700

    convert input into integer

--- firstwebsite.html/js/dynamic.js
+++ firstwebsite.html/js/dynamic.js
@@ -24,17 +24,17 @@ else {
 
 
 var numIcecream = prompt("Choose a number from 1 to 10 to tell me how many icecreams  you want. And I will tell you wich flavors you will get:");
-numIcecream = parseInt(numIcecream);
+num = parseInt(numIcecream);
 var flavors = ['vanilla', 'chocolate', 'strawberry', 'mango', 'pinapple', 'watermelon', 'cherry', 'bubblegum', 'chocolate mint', 'cookie dough'];
 var answr = '';
 
 for (var i = 1; i < 11; i++) {
-    if (numIcecream !== i) {
-        alert("Opps, Please try again any a number from 1 to 10");
+    if (num !== i) {
+        numIcecream = prompt("Opps, Please try again any a number from 1 to 10");
     }
     else {
         for ( var j = 0; j < flavors.length; j++) {
-            answr += flavors[j] + 'ice cream flavor';
+            answr += flavors[j] + ' ice cream flavor ,';
         }
         alert(answr);
     }
