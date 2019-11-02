0aa97a66b877d459bc61e402d25f0481c0b2f64b
daesy13 <daesyramos@hotmail.com>
2019-11-01 23:01:36 -0700

    fixing conditional for letter p

--- firstwebsite.html/js/dynamic.js
+++ firstwebsite.html/js/dynamic.js
@@ -2,7 +2,7 @@ window.addEventListener("keydown", event => {
     if (event.key == "p") {
         document.body.style.background = "purple";
     }
-    else if (event.key != "p") {
+    else if (event.key !== "p") {
         alert('Hey, that is not letter "p"!!!')
     }
 });
