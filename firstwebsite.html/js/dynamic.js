465bdaec7860686381c4e5d92cd0d20b3c37184e
daesy13 <daesyramos@hotmail.com>
2019-11-02 13:56:21 -0700

    adding nested loop

--- firstwebsite.html/js/dynamic.js
+++ firstwebsite.html/js/dynamic.js
@@ -26,15 +26,17 @@ else {
 var numIcecream = prompt("Choose a number from 1 to 10 to tell me how many icecreams  you want. And I will tell you wich flavors you will get:");
 num = parseInt(numIcecream);
 var flavors = ['vanilla', 'chocolate', 'strawberry', 'mango', 'pinapple', 'watermelon', 'cherry', 'bubblegum', 'chocolate mint', 'cookie dough'];
-var answr = '';
+var answr = 'You will get :';
 
 for (var i = 1; i < 11; i++) {
     if (num !== i) {
         numIcecream = prompt("Opps, Please try again any a number from 1 to 10");
     }
     else {
-        for ( var j = 0; j < flavors.length; j++) {
-            answr += flavors[j] + ' ice cream flavor ,';
+        for (var k = 1; k < num + 1; k++){
+            for ( var j = 0; j < flavors.length; j++) {
+                answr += flavors[j] + ' ice cream flavor ,';
+            }
         }
         alert(answr);
     }
